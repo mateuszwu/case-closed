6deee94d77a2904a48f05b7bfe0f3b7b1c613752
Mateusz Wieczorek <mateusz.wu@gmail.com>
2018-06-12 08:08:45 +0200

    postgresql setup

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171031121832) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cases", force: :cascade do |t|
     t.string "name"
     t.string "case_uid"
